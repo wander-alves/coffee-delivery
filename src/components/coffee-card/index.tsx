2d06560ba93e33981ad81d598db5f68ec5ed3700
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-11-26 00:26:01 -0300

    feat: adding product card component

---
+++ src/components/coffee-card/index.tsx
@@ -0,0 +1,29 @@
+import { ShoppingCartSimpleIcon } from "@phosphor-icons/react";
+import { CardControlsContainer, CardPriceAndControlsContainer, CoffeeCardContainer, TagItem, TagList } from "./styles";
+
+import coffeeImage from '../../assets/images/americano.png';
+import { CartControls } from "../cart-controls";
+
+export function CoffeeCard (){
+  return (
+    <CoffeeCardContainer>
+      <img src={coffeeImage} />
+      <TagList>
+        <TagItem>
+          Tradicional
+        </TagItem>
+      </TagList>
+      <h2>Expresso Americano</h2>
+      <p>Expresso diluído, menos intenso que o tradicional</p>
+      <CardPriceAndControlsContainer>
+        <span>R$ <strong>9,90</strong></span>
+        <CardControlsContainer>
+          <CartControls />          
+          <button>
+            <ShoppingCartSimpleIcon size={22} weight="fill"/>
+          </button>
+        </CardControlsContainer>
+      </CardPriceAndControlsContainer>
+    </CoffeeCardContainer>
+  )
+}
---
+++ src/components/coffee-card/styles.ts
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+export const CoffeeCardContainer = styled.div`
+  padding: 2rem;
+  width: 25.6rem;
+  height: 31rem;
+
+  border-radius: 6px;
+  border-top-right-radius: 36px;
+  border-bottom-left-radius: 36px;
+
+  background-color: ${({theme})=> theme.colors["base-card"]};
+
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &>img{
+    position: absolute;
+    top: -2rem;
+    width: 12rem;
+    height: 12rem;
+  }
+  &>h2 { 
+    font: ${({theme})=> theme.fonts["title-s"]};
+    color: ${({theme})=> theme.colors["base-subtitle"]};
+    margin-block: 1.6rem 0.8rem;
+  }
+  &>p {
+    text-align: center;
+    font: ${({theme})=> theme.fonts["text-s"]};
+    color: ${({theme})=> theme.colors["base-label"]};
+    margin-bottom: 3.2rem;
+  }
+`;
+
+export const TagList = styled.div`
+  display: flex;
+  gap: 0.4rem;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 9.2rem;
+`;
+export const TagItem = styled.div`
+  padding: 0.4rem 0.8rem;
+  border-radius: 999px;
+  font: ${({theme})=> theme.fonts.tag};
+  text-transform: uppercase;
+  background-color: ${({theme})=> theme.colors["yellow-light"]};
+  color: ${({theme})=> theme.colors["yellow-dark"]};
+`;
+
+export const CardPriceAndControlsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  
+  &>span {
+    font: ${({theme})=> theme.fonts["text-s"]};
+    color: ${({theme})=> theme.colors["base-text"]};
+    
+    &>strong {
+      font: ${({theme})=> theme.fonts["title-m"]};
+    }
+  }
+`;
+
+export const CardControlsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+
+  & button{
+    padding: 0.8rem;
+    border-radius: 6px;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+
+    &>svg {
+      fill: ${({theme})=> theme.colors["base-card"]};
+    }
+  }
+  
+  &>button {
+    background-color: ${({theme})=> theme.colors["purple-dark"]};
+  }
+  
+  &>div{ 
+    display: flex;
+    height: 100%;
+    background-color: ${({theme})=> theme.colors["base-button"]};
+    border-radius: 6px;
+
+    &>button>svg{
+      fill: ${({theme})=> theme.colors.purple};
+    }
+    &>span { 
+      min-width: 2rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`;
